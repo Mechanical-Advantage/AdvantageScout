5343bcd2f932c5a68c5463ff302676cfc6080932
Jonah Bonner <jonahwbonner@gmail.com>
2019-07-30 09:51:11 -0400

    Add release shortcut and removed platforms directory

---
+++ cordova/release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Get app version
+raw=`cat config.xml`
+raw=`echo "$raw" | tr '\n' '_'`
+IFS='"' read -a split <<< "${raw}"
+version="${split[3]}"
+
+#Build android app
+cordova build android --release
+
+#Copy apk to releases folder
+cp "platforms/android/app/build/outputs/apk/release/app-release.apk" "releases/AdvantageScout ${version}.apk"
+echo "AdvantageScout ${version} copied to \"releases\" folder"
