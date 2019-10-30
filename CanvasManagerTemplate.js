6903e498e0d64935932a8fd54a9430fa5b532999
Jonah Bonner <jonahwbonner@gmail.com>
2019-10-30 18:22:40 -0400

    Add canvas manager template

---
+++ CanvasManagerTemplate.js
@@ -0,0 +1,24 @@
+// canvas object in variable 'canvas'
+// alliances reversed in variable 'reverseAlliances' -> no reverse = red on right
+// width = 3000px, height = 1600px
+var mode = 0 // 0 = auto, 1 = teleop, 2 = endgame
+this.setMode = function(newMode) { // REQUIRED FUNCTION
+    mode = newMode
+    render()
+}
+this.getData = function() { // REQUIRED FUNCTION
+    return data
+}
+function uploadData() { // Closes scouting interface and saves data (if using visual for end game, must have a call to this function)
+    document.dispatchEvent(uploadEvent)
+}
+
+var context = canvas.getContext("2d")
+var buttonManager = new ButtonManager(canvas)
+var data = {}
+
+function render() {
+    context.clearRect(0, 0, 3000, 1600)
+    }
+}
+render()
