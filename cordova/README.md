575506b388fff9f972f7077b8e500ff2f836fbab
Jonah Bonner <jonahwbonner@gmail.com>
2020-01-04 20:11:19 -0500

    Add support for number fields

--- cordova/README.md
+++ cordova/README.md
@@ -7,6 +7,9 @@ The app is not available on the Google Play Store, so it must be installed one o
 2. Use the short link http://bit.ly/6328scout
 
 ## Changelog
+### 1.4.4
+* Added support for number fields
+
 ### 1.4.3
 * Added alert when forwarding server cannot reach main server.
 
--- src/ClassicManager.js
+++ src/ClassicManager.js
@@ -4,9 +4,9 @@ function ClassicManager(appManager) {
     var fieldPrefsLookup = {} // prefs for each field (min, max, step, etc.)
     var fieldModeLists = [[], [], [], []] // lists of which fields are in which mode
     var counterData = {} // keeps track of counter values
-    
+
     // Set up classic interface
-    this.start = function() {
+    this.start = function () {
         fieldPrefsLookup = {}
         fieldModeLists = [[], [], [], []]
         var mainDivs = [document.getElementById("classicDiv1"), document.getElementById("classicDiv2"), document.getElementById("classicDiv3"), document.getElementById("pitClassicDiv")]
@@ -20,7 +20,7 @@ function ClassicManager(appManager) {
             while (mainDivs[mode].firstChild) {
                 mainDivs[mode].removeChild(mainDivs[mode].firstChild)
             }
-            
+
             var inputDataList = appManager.game.prefs.classic[modeLookup[mode]]
             for (var inputNumber = 0; inputNumber < inputDataList.length; inputNumber++) {
                 var inputData = inputDataList[inputNumber]
@@ -34,7 +34,7 @@ function ClassicManager(appManager) {
                     unit.firstChild.firstChild.classList.add("classiclabel")
                     unit.firstChild.firstChild.style.textDecoration = "underline"
                     unit.firstChild.firstChild.innerHTML = inputData.label
-                    
+
                     unit.appendChild(document.createElement("DIV"))
                     unit.children[1].classList.add("classicgroup_units")
                     unit.children[1].appendChild(unitFor(inputData.unit1, false, modeLookup[mode]))
@@ -55,7 +55,7 @@ function ClassicManager(appManager) {
             }
         }
     }
-    
+
     // Get HTML for unit
     function unitFor(inputData, wideAllowed, modeName) {
         var unit = document.createElement("DIV")
@@ -72,10 +72,10 @@ function ClassicManager(appManager) {
         } else {
             unit.firstChild.firstChild.innerHTML = inputData.label
         }
-        
+
         unit.appendChild(document.createElement("DIV"))
         unit.children[1].classList.add("classiccontrols")
-        
+
         if (inputData.type == "chooser") {
             var select = document.createElement("SELECT")
             unit.children[1].appendChild(select)
@@ -93,32 +93,38 @@ function ClassicManager(appManager) {
             textarea.classList.add("classicinput")
             textarea.id = modeName + "_" + inputData.field
             textarea.placeholder = "Enter text here..."
+        } else if (inputData.type == "number") {
+            var input = document.createElement("INPUT")
+            unit.children[1].appendChild(input)
+            input.classList.add("classicinput")
+            input.id = modeName + "_" + inputData.field
+            input.type = "number"
         } else if (inputData.type == "counter") {
             var downButton = document.createElement("BUTTON")
             unit.children[1].appendChild(downButton)
             downButton.classList.add("classiccounter_button")
             downButton.classList.add("classiccounter_down")
-            downButton.onclick = function() {
+            downButton.onclick = function () {
                 var field = this.parentElement.children[1].id
                 if (fieldPrefsLookup[field].min < counterData[field]) {
                     counterData[field] -= fieldPrefsLookup[field].step
                     this.parentElement.children[1].innerHTML = counterData[field]
                 }
             }
-            
+
             var number = document.createElement("DIV")
             unit.children[1].appendChild(number)
             number.classList.add("classiccounter_number")
             number.innerHTML = inputData.min
             counterData[modeName + "_" + inputData.field] = inputData.min
-            fieldPrefsLookup[modeName + "_" + inputData.field] = {"min": inputData.min, "max": inputData.max, "step": inputData.step}
+            fieldPrefsLookup[modeName + "_" + inputData.field] = { "min": inputData.min, "max": inputData.max, "step": inputData.step }
             number.id = modeName + "_" + inputData.field
-            
+
             var upButton = document.createElement("BUTTON")
             unit.children[1].appendChild(upButton)
             upButton.classList.add("classiccounter_button")
             upButton.classList.add("classiccounter_up")
-            upButton.onclick = function() {
+            upButton.onclick = function () {
                 var field = this.parentElement.children[1].id
                 if (fieldPrefsLookup[field].max > counterData[field]) {
                     counterData[field] += fieldPrefsLookup[field].step
@@ -132,7 +138,7 @@ function ClassicManager(appManager) {
             checkbox.classList.add("classiccheck")
             checkbox.id = modeName + "_" + inputData.field
             checkbox.style.backgroundColor = "#ff7575"
-            checkbox.onclick = function() {
+            checkbox.onclick = function () {
                 if (this.style.backgroundColor == "rgb(255, 117, 117)") {
                     this.style.backgroundColor = "#75ff91"
                 } else {
@@ -146,7 +152,7 @@ function ClassicManager(appManager) {
             unit.children[1].appendChild(button)
             button.classList.add("classicuploadbutton")
             button.innerHTML = "Save"
-            button.onclick = function() {
+            button.onclick = function () {
                 appManager.scoutManager.upload()
             }
         } else if (inputData.type == "image") {
@@ -156,22 +162,22 @@ function ClassicManager(appManager) {
             button.style.backgroundColor = "#ff7575"
             button.innerHTML = "Camera"
             button.id = modeName + "_" + inputData.field
-            button.onclick = function() {
+            button.onclick = function () {
                 appManager.classicManager.takePhoto(this)
             }
         }
         return unit
     }
-    
+
     // Retrieve data from document
-    this.getData = function(scoutMode) {
+    this.getData = function (scoutMode) {
         var result = {}
         var useResult
         if (scoutMode == "classic") {
             useResult = [true, true, true, false]
         } else if (scoutMode == "pit") {
             useResult = [false, false, false, true]
-        } else{
+        } else {
             useResult = [appManager.game.prefs.forceClassic.auto, appManager.game.prefs.forceClassic.teleop, appManager.game.prefs.forceClassic.endgame, false]
         }
         for (var mode = 0; mode < 4; mode++) {
@@ -179,7 +185,7 @@ function ClassicManager(appManager) {
                 for (var i = 0; i < fieldModeLists[mode].length; i++) {
                     var fieldName = fieldModeLists[mode][i]
                     var input = document.getElementById(modeLookup[mode] + "_" + fieldName)
-                    
+
                     if (input.tagName == "DIV") {
                         result[fieldName] = Number(input.innerText)
                     } else if (input.type == "select-one") {
@@ -188,9 +194,11 @@ function ClassicManager(appManager) {
                         } else {
                             result[fieldName] = Number(input.value)
                         }
-                        
+
                     } else if (input.type == "textarea") {
                         result[fieldName] = input.value
+                    } else if (input.type == "number") {
+                        result[fieldName] = Number(input.value)
                     } else if (input.type == "submit") {
                         if (input.innerHTML == "Camera") {
                             result[fieldName] = input.image
@@ -207,9 +215,9 @@ function ClassicManager(appManager) {
         }
         return result
     }
-    
+
     // Take photo and return file path
-    this.takePhoto = function(button) {
+    this.takePhoto = function (button) {
         if (appManager.web) {
             appManager.notificationManager.alert("Not supported", "Photos are not supported right now.")
         } else {
@@ -224,7 +232,7 @@ function ClassicManager(appManager) {
                 button.image = imageUrl
                 button.style.backgroundColor = "#75ff91"
             }
-            navigator.camera.getPicture(onSuccess, function() {}, cameraOptions)
+            navigator.camera.getPicture(onSuccess, function () { }, cameraOptions)
         }
     }
 }
