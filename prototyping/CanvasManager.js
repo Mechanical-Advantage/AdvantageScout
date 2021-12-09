ae39c140c135d53a926fea5b033c58beb2ea3c55
Ayush <ayushk2023@gmail.com>
2021-12-08 20:47:30 -0500

    start 2012 game

--- prototyping/CanvasManager.js
+++ prototyping/CanvasManager.js
@@ -2,34 +2,38 @@
 // width = 3000px, height = 1600px
 var context = canvas.getContext("2d")
 var buttonManager = new ButtonManager(canvas)
-var Changecolor = "#0000FF" 
+var green = "#41d936" 
 function render() {
+    //hoop points
     context.clearRect(0, 0, 3000, 1600)
-    context.fillStyle = "#FF0000"
-    context.fillRect(100, 100, 300, 300)
-    context.fillStyle = Changecolor
-    context.fillRect(1300, 600, 400, 400)
-    context.beginPath()
-    context.moveTo(500, 500)
-    context.lineTo(500, 700)
-    context.moveTo(600, 500)
-    context.lineTo(600, 700)
-    context.moveTo(400, 500)
-    context.lineTo(700, 500)
+    context.fillStyle = green
+    context.rect(100, 100, 200, 400) //1
+    context.fillRect(100, 100, 200, 400) //1
+
+    context.rect(100, 600, 200, 400) //2
+    context.fillRect(100, 600, 200, 400) //2
+
+    context.rect(100, 1100, 200, 400) //3
+    context.fillRect(100, 1100, 200, 400) //3
+    
+    context.rect(300, 100, 200, 400)//1.5
+    context.rect(300, 600, 200, 400)//2.5
+    context.rect(300, 1100, 200, 400)//3.5
+    //first button divider
+    context.moveTo(300, 100)
+    context.lineTo(300, 500)
+
+    //second button divider
+    context.moveTo(300, 600)
+    context.lineTo(300, 1000)
+
+    //third button divider
+    context.moveTo(300, 1100)
+    context.lineTo(300, 1500)
+
     context.lineWidth = 10
     context.stroke()
 
 }
 
-buttonManager.addButton("testButton", new Button(100, 100, 300, 300, function() {
-                                                 alert("You clicked the button!")
-                                                 }))
-buttonManager.addButton("TestButton2", new Button(1300, 600, 400, 400, function() {    
-    Changecolor = "#FFFF00"
-    render()
-    alert("something amuzing")
-}))
-buttonManager.addButton("TestButton3", new Button(400, 500, 200, 200, function() {
-    alert("This is pi")
-}))
 render()
