83d366b87e4ef7a54330ba77f4c7ee226bd2966d
V-RA <23vinamra@gmail.com>
2022-11-14 20:56:59 -0500

    work on front-end for app
    Co-authored-by: manthan-acharya <manthan-acharya@users.noreply.github.com>

--- games/config/2022/GameComponent.svelte
+++ games/config/2022/GameComponent.svelte
@@ -1,7 +1,5 @@
-<h3>This is Svelte component, packaged with the game.</h3>
-
-<style>
-    h3 {
-        font-style: italic;
-    }
-</style>
+<script>
+    import AutoPage from "./AutoPage.svelte";
+    
+</script>
+<AutoPage />
