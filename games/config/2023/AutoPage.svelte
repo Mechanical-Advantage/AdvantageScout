c9d92d88790f0a2207a49fd9c625929580a63f65
Scout <6328scout1@gmail.com>
2023-01-17 21:45:28 -0500

    Adjust cones for Kindle

--- games/config/2023/AutoPage.svelte
+++ games/config/2023/AutoPage.svelte
@@ -6,49 +6,247 @@
     <!--checkbox and cross-->
     <div class="btn group">
         <button class="btn btn-square btn-outline">
-            <svg fill="#1bbb43" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#1bbb43"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <polygon fill-rule="evenodd" points="9.707 14.293 19 5 20.414 6.414 9.707 17.121 4 11.414 5.414 10"></polygon> </g></svg>
+            <svg
+                fill="#1bbb43"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#1bbb43"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier">
+                    <polygon
+                        fill-rule="evenodd"
+                        points="9.707 14.293 19 5 20.414 6.414 9.707 17.121 4 11.414 5.414 10"
+                    />
+                </g></svg
+            >
         </button>
         <button class="btn btn-square btn-outline">
-            <svg fill="#e31c1c" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="cross" class="icon glyph" stroke="#e31c1c"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></g></svg>
+            <svg
+                fill="#e31c1c"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                id="cross"
+                class="icon glyph"
+                stroke="#e31c1c"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier"
+                    ><path
+                        d="M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"
+                    /></g
+                ></svg
+            >
         </button>
     </div>
     <div class="btn group">
         <button class="btn btn-square btn-outline">
-            <svg fill="#1bbb43" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#1bbb43"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <polygon fill-rule="evenodd" points="9.707 14.293 19 5 20.414 6.414 9.707 17.121 4 11.414 5.414 10"></polygon> </g></svg>
+            <svg
+                fill="#1bbb43"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#1bbb43"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier">
+                    <polygon
+                        fill-rule="evenodd"
+                        points="9.707 14.293 19 5 20.414 6.414 9.707 17.121 4 11.414 5.414 10"
+                    />
+                </g></svg
+            >
         </button>
         <button class="btn btn-square btn-outline">
-            <svg fill="#e31c1c" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="cross" class="icon glyph" stroke="#e31c1c"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></g></svg>
+            <svg
+                fill="#e31c1c"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                id="cross"
+                class="icon glyph"
+                stroke="#e31c1c"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier"
+                    ><path
+                        d="M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"
+                    /></g
+                ></svg
+            >
         </button>
     </div>
     <div class="btn group">
         <button class="btn btn-square btn-outline">
-            <svg fill="#1bbb43" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#1bbb43"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <polygon fill-rule="evenodd" points="9.707 14.293 19 5 20.414 6.414 9.707 17.121 4 11.414 5.414 10"></polygon> </g></svg>
+            <svg
+                fill="#1bbb43"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#1bbb43"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier">
+                    <polygon
+                        fill-rule="evenodd"
+                        points="9.707 14.293 19 5 20.414 6.414 9.707 17.121 4 11.414 5.414 10"
+                    />
+                </g></svg
+            >
         </button>
         <button class="btn btn-square btn-outline">
-            <svg fill="#e31c1c" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="cross" class="icon glyph" stroke="#e31c1c"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></g></svg>
+            <svg
+                fill="#e31c1c"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                id="cross"
+                class="icon glyph"
+                stroke="#e31c1c"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier"
+                    ><path
+                        d="M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"
+                    /></g
+                ></svg
+            >
         </button>
     </div>
 
     <!--cone and cube-->
-    <div class="ml-[650px] mt-[300px]">
+    <div class="ml-[650px] mt-[100px]">
         <button class="btn btn-outline btn-warning btn-square btn-lg">
-            <svg class="-mt-2.5 bi bi-cone" width="82px" height="82px" viewBox="0 0 16.00 16.00" xmlns="http://www.w3.org/2000/svg" fill="#dccc18" stroke="#dccc18"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7.03 1.88c.252-1.01 1.688-1.01 1.94 0l2.905 11.62H14a.5.5 0 0 1 0 1H2a.5.5 0 0 1 0-1h2.125L7.03 1.88z"></path> </g></svg>
+            <svg
+                class="-mt-2.5 bi bi-cone"
+                width="82px"
+                height="82px"
+                viewBox="0 0 16.00 16.00"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#dccc18"
+                stroke="#dccc18"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier">
+                    <path
+                        d="M7.03 1.88c.252-1.01 1.688-1.01 1.94 0l2.905 11.62H14a.5.5 0 0 1 0 1H2a.5.5 0 0 1 0-1h2.125L7.03 1.88z"
+                    />
+                </g></svg
+            >
         </button>
         <button class="btn btn-outline btn-primary btn-square btn-lg">
-            <svg class="-mt-2.5" fill="#c729c2" width="82px" height="82px" viewBox="0 0 56 56" xmlns="http://www.w3.org/2000/svg" stroke="#c729c2"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M 28.0000 26.6406 L 50.0783 14.1016 C 49.7264 13.75 49.3045 13.4688 48.7890 13.1875 L 32.2657 3.7657 C 30.8126 2.9453 29.4063 2.5000 28.0000 2.5000 C 26.5938 2.5000 25.1875 2.9453 23.7344 3.7657 L 7.2110 13.1875 C 6.6954 13.4688 6.2735 13.75 5.9219 14.1016 Z M 26.4063 53.5 L 26.4063 29.4532 L 4.3985 16.8906 C 4.2813 17.4063 4.2110 17.9688 4.2110 18.6719 L 4.2110 36.9297 C 4.2110 40.3281 5.4063 41.5938 7.5860 42.8360 L 25.9375 53.2891 C 26.1016 53.3828 26.2422 53.4532 26.4063 53.5 Z M 29.5938 53.5 C 29.7579 53.4532 29.8985 53.3828 30.0626 53.2891 L 48.4141 42.8360 C 50.5938 41.5938 51.7890 40.3281 51.7890 36.9297 L 51.7890 18.6719 C 51.7890 17.9688 51.7189 17.4063 51.6018 16.8906 L 29.5938 29.4532 Z"></path></g></svg>        </button>
+            <svg
+                class="-mt-2.5"
+                fill="#c729c2"
+                width="82px"
+                height="82px"
+                viewBox="0 0 56 56"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#c729c2"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier"
+                    ><path
+                        d="M 28.0000 26.6406 L 50.0783 14.1016 C 49.7264 13.75 49.3045 13.4688 48.7890 13.1875 L 32.2657 3.7657 C 30.8126 2.9453 29.4063 2.5000 28.0000 2.5000 C 26.5938 2.5000 25.1875 2.9453 23.7344 3.7657 L 7.2110 13.1875 C 6.6954 13.4688 6.2735 13.75 5.9219 14.1016 Z M 26.4063 53.5 L 26.4063 29.4532 L 4.3985 16.8906 C 4.2813 17.4063 4.2110 17.9688 4.2110 18.6719 L 4.2110 36.9297 C 4.2110 40.3281 5.4063 41.5938 7.5860 42.8360 L 25.9375 53.2891 C 26.1016 53.3828 26.2422 53.4532 26.4063 53.5 Z M 29.5938 53.5 C 29.7579 53.4532 29.8985 53.3828 30.0626 53.2891 L 48.4141 42.8360 C 50.5938 41.5938 51.7890 40.3281 51.7890 36.9297 L 51.7890 18.6719 C 51.7890 17.9688 51.7189 17.4063 51.6018 16.8906 L 29.5938 29.4532 Z"
+                    /></g
+                ></svg
+            >
+        </button>
     </div>
-    <div class="ml-[900px] mt-[150px]">
+    <div class="ml-[825px] mt-[50px]">
         <button class="btn btn-outline btn-warning btn-square btn-lg">
-            <svg class="-mt-2.5 bi bi-cone" width="82px" height="82px" viewBox="0 0 16.00 16.00" xmlns="http://www.w3.org/2000/svg" fill="#dccc18" stroke="#dccc18"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7.03 1.88c.252-1.01 1.688-1.01 1.94 0l2.905 11.62H14a.5.5 0 0 1 0 1H2a.5.5 0 0 1 0-1h2.125L7.03 1.88z"></path> </g></svg>
+            <svg
+                class="-mt-2.5 bi bi-cone"
+                width="82px"
+                height="82px"
+                viewBox="0 0 16.00 16.00"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#dccc18"
+                stroke="#dccc18"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier">
+                    <path
+                        d="M7.03 1.88c.252-1.01 1.688-1.01 1.94 0l2.905 11.62H14a.5.5 0 0 1 0 1H2a.5.5 0 0 1 0-1h2.125L7.03 1.88z"
+                    />
+                </g></svg
+            >
         </button>
         <button class="btn btn-outline btn-primary btn-square btn-lg">
-            <svg class="-mt-2.5" fill="#c729c2" width="82px" height="82px" viewBox="0 0 56 56" xmlns="http://www.w3.org/2000/svg" stroke="#c729c2"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M 28.0000 26.6406 L 50.0783 14.1016 C 49.7264 13.75 49.3045 13.4688 48.7890 13.1875 L 32.2657 3.7657 C 30.8126 2.9453 29.4063 2.5000 28.0000 2.5000 C 26.5938 2.5000 25.1875 2.9453 23.7344 3.7657 L 7.2110 13.1875 C 6.6954 13.4688 6.2735 13.75 5.9219 14.1016 Z M 26.4063 53.5 L 26.4063 29.4532 L 4.3985 16.8906 C 4.2813 17.4063 4.2110 17.9688 4.2110 18.6719 L 4.2110 36.9297 C 4.2110 40.3281 5.4063 41.5938 7.5860 42.8360 L 25.9375 53.2891 C 26.1016 53.3828 26.2422 53.4532 26.4063 53.5 Z M 29.5938 53.5 C 29.7579 53.4532 29.8985 53.3828 30.0626 53.2891 L 48.4141 42.8360 C 50.5938 41.5938 51.7890 40.3281 51.7890 36.9297 L 51.7890 18.6719 C 51.7890 17.9688 51.7189 17.4063 51.6018 16.8906 L 29.5938 29.4532 Z"></path></g></svg>        </button>
-    </div> 
-    <div class="ml-[750px] mt-[150px]">
+            <svg
+                class="-mt-2.5"
+                fill="#c729c2"
+                width="82px"
+                height="82px"
+                viewBox="0 0 56 56"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#c729c2"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier"
+                    ><path
+                        d="M 28.0000 26.6406 L 50.0783 14.1016 C 49.7264 13.75 49.3045 13.4688 48.7890 13.1875 L 32.2657 3.7657 C 30.8126 2.9453 29.4063 2.5000 28.0000 2.5000 C 26.5938 2.5000 25.1875 2.9453 23.7344 3.7657 L 7.2110 13.1875 C 6.6954 13.4688 6.2735 13.75 5.9219 14.1016 Z M 26.4063 53.5 L 26.4063 29.4532 L 4.3985 16.8906 C 4.2813 17.4063 4.2110 17.9688 4.2110 18.6719 L 4.2110 36.9297 C 4.2110 40.3281 5.4063 41.5938 7.5860 42.8360 L 25.9375 53.2891 C 26.1016 53.3828 26.2422 53.4532 26.4063 53.5 Z M 29.5938 53.5 C 29.7579 53.4532 29.8985 53.3828 30.0626 53.2891 L 48.4141 42.8360 C 50.5938 41.5938 51.7890 40.3281 51.7890 36.9297 L 51.7890 18.6719 C 51.7890 17.9688 51.7189 17.4063 51.6018 16.8906 L 29.5938 29.4532 Z"
+                    /></g
+                ></svg
+            >
+        </button>
+    </div>
+    <div class="ml-[650px]">
         <button class="btn btn-outline btn-warning btn-square btn-lg">
-            <svg class="-mt-2.5 bi bi-cone" width="82px" height="82px" viewBox="0 0 16.00 16.00" xmlns="http://www.w3.org/2000/svg" fill="#dccc18" stroke="#dccc18"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7.03 1.88c.252-1.01 1.688-1.01 1.94 0l2.905 11.62H14a.5.5 0 0 1 0 1H2a.5.5 0 0 1 0-1h2.125L7.03 1.88z"></path> </g></svg>
+            <svg
+                class="-mt-2.5 bi bi-cone"
+                width="82px"
+                height="82px"
+                viewBox="0 0 16.00 16.00"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#dccc18"
+                stroke="#dccc18"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier">
+                    <path
+                        d="M7.03 1.88c.252-1.01 1.688-1.01 1.94 0l2.905 11.62H14a.5.5 0 0 1 0 1H2a.5.5 0 0 1 0-1h2.125L7.03 1.88z"
+                    />
+                </g></svg
+            >
         </button>
         <button class="btn btn-outline btn-primary btn-square btn-lg">
-            <svg class="-mt-2.5" fill="#c729c2" width="82px" height="82px" viewBox="0 0 56 56" xmlns="http://www.w3.org/2000/svg" stroke="#c729c2"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M 28.0000 26.6406 L 50.0783 14.1016 C 49.7264 13.75 49.3045 13.4688 48.7890 13.1875 L 32.2657 3.7657 C 30.8126 2.9453 29.4063 2.5000 28.0000 2.5000 C 26.5938 2.5000 25.1875 2.9453 23.7344 3.7657 L 7.2110 13.1875 C 6.6954 13.4688 6.2735 13.75 5.9219 14.1016 Z M 26.4063 53.5 L 26.4063 29.4532 L 4.3985 16.8906 C 4.2813 17.4063 4.2110 17.9688 4.2110 18.6719 L 4.2110 36.9297 C 4.2110 40.3281 5.4063 41.5938 7.5860 42.8360 L 25.9375 53.2891 C 26.1016 53.3828 26.2422 53.4532 26.4063 53.5 Z M 29.5938 53.5 C 29.7579 53.4532 29.8985 53.3828 30.0626 53.2891 L 48.4141 42.8360 C 50.5938 41.5938 51.7890 40.3281 51.7890 36.9297 L 51.7890 18.6719 C 51.7890 17.9688 51.7189 17.4063 51.6018 16.8906 L 29.5938 29.4532 Z"></path></g></svg>        </button>
+            <svg
+                class="-mt-2.5"
+                fill="#c729c2"
+                width="82px"
+                height="82px"
+                viewBox="0 0 56 56"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#c729c2"
+                ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                /><g id="SVGRepo_iconCarrier"
+                    ><path
+                        d="M 28.0000 26.6406 L 50.0783 14.1016 C 49.7264 13.75 49.3045 13.4688 48.7890 13.1875 L 32.2657 3.7657 C 30.8126 2.9453 29.4063 2.5000 28.0000 2.5000 C 26.5938 2.5000 25.1875 2.9453 23.7344 3.7657 L 7.2110 13.1875 C 6.6954 13.4688 6.2735 13.75 5.9219 14.1016 Z M 26.4063 53.5 L 26.4063 29.4532 L 4.3985 16.8906 C 4.2813 17.4063 4.2110 17.9688 4.2110 18.6719 L 4.2110 36.9297 C 4.2110 40.3281 5.4063 41.5938 7.5860 42.8360 L 25.9375 53.2891 C 26.1016 53.3828 26.2422 53.4532 26.4063 53.5 Z M 29.5938 53.5 C 29.7579 53.4532 29.8985 53.3828 30.0626 53.2891 L 48.4141 42.8360 C 50.5938 41.5938 51.7890 40.3281 51.7890 36.9297 L 51.7890 18.6719 C 51.7890 17.9688 51.7189 17.4063 51.6018 16.8906 L 29.5938 29.4532 Z"
+                    /></g
+                ></svg
+            >
+        </button>
     </div>
 </main>
