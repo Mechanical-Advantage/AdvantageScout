9abb8e24a716b46306083e4ed84d6a66ced3617d
Manthan Acharya <manthan.boston@gmail.com>
2023-03-06 21:38:39 -0500

    Update slack notifications

--- slack_schedule_send_v2.py
+++ slack_schedule_send_v2.py
@@ -9,7 +9,7 @@ load_dotenv()
 slack_token = os.getenv('SLACK_TOKEN')
 
 db_global = "global.db"
-
+test_users = ['UJCC5DAF6', 'UJD1564E9']
 client = WebClient(token=slack_token)
 conn_global = sql.connect(db_global)
 cur_global = conn_global.cursor()
@@ -46,7 +46,7 @@ for scout in scoutBreakInfo:
     breaks = scoutBreakInfo[scout]
     try:
         slackUid = cur_global.execute(
-            "select userid from Slack_UserIDs where name = ? ", (scoutName,)).fetchall()[0][0]
+            "select userid from Slack_UserIDs where name = ? ", (scout,)).fetchall()[0][0]
 
     except:
         slackUid = "none"
@@ -62,9 +62,11 @@ for scout in scoutBreakInfo:
             str(breakStart) + " ending on match " + str(breakEnd)+"\r\n"
 
     print(scout, msgText)
-    # try:
-    #     response = client.chat_postMessage(
-    #         channel=slackUid,
-    #         text=msgText)
-    # except:
-    #     print("Message to ", scout, " failed")
+    if (slackUid in test_users):
+        print("sending via slack")
+        try:
+            response = client.chat_postMessage(
+                channel=slackUid,
+                text=msgText)
+        except:
+            print("Message to ", scout, " failed")
