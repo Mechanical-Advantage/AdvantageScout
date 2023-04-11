546b5eb29d44bf600d8f40b3b51f2b659493120d
Manthan Acharya <manthan.boston@gmail.com>
2023-04-11 19:12:53 -0400

    Fix Device State on Admin Page!!

--- admin/src/Devices.svelte
+++ admin/src/Devices.svelte
@@ -4,17 +4,14 @@
 
     let devices = [];
 
-     async function getData(){
-        
+    async function getData() {
         const response = await fetch("/get_devices", { method: "GET" });
         const data = await response.json();
         devices = data;
         //console.log("Data" + data)
-    
-}
-setInterval(getData, 1500)
+    }
+    setInterval(getData, 1500);
 
-    
     // if device.last_battery is between 70 - 100, bg-green-500
     // if device.last_battery is between 30 - 69, bg-yellow-500
     // if device.last_battery is between 0 - 29, bg-red-500
@@ -39,39 +36,41 @@ setInterval(getData, 1500)
     };
 
     const scoutStatus = (status) => {
-        if (status == 0) {
+        if (status == 1) {
             return "Auto";
-        } else if (status == 1) {
-            return "Teleop";
         } else if (status == 2) {
-            return "Endgame";
+            return "Teleop";
         } else if (status == 3) {
+            return "Endgame";
+        } else if (status == 4) {
+            return "Offline";
+        } else if (status == 5) {
             return "Pit Scout";
         }
     };
 
     function formattedTime(unixTime) {
-        let diff = Math.round(Date.now() / 1000) - unixTime
-        let hours = Math.floor(diff / 3600)
-        diff -= hours * 3600
-        let minutes = Math.floor(diff / 60)
-        let seconds = diff - minutes * 60
+        let diff = Math.round(Date.now() / 1000) - unixTime;
+        let hours = Math.floor(diff / 3600);
+        diff -= hours * 3600;
+        let minutes = Math.floor(diff / 60);
+        let seconds = diff - minutes * 60;
 
-        let formatted = ""
+        let formatted = "";
         if (hours != 0) {
-            formatted = String(hours) + "h "
+            formatted = String(hours) + "h ";
         }
         if (minutes != 0) {
-            formatted = formatted + String(minutes) + "m "
+            formatted = formatted + String(minutes) + "m ";
         }
         if (seconds != 0) {
-            formatted = formatted + String(seconds) + "s "
+            formatted = formatted + String(seconds) + "s ";
         }
         formatted = formatted.substring(0, formatted.length - 1);
         if (formatted == "") {
-            formatted = "0s"
+            formatted = "0s";
         }
-        return formatted
+        return formatted;
     }
 </script>
 
@@ -93,9 +92,16 @@ setInterval(getData, 1500)
             <tr>
                 <td>{device.name}</td>
                 <td
-                    class={Math.round(Date.now() / 1000) - device.last_heartbeat > 30
+                    class={Math.round(Date.now() / 1000) -
+                        device.last_heartbeat >
+                    30
                         ? "bg-red-500"
-                        : Math.round(Date.now() / 1000) - device.last_heartbeat < 10 ? "bg-green-500" : "bg-yellow-500" }>{formattedTime(device.last_heartbeat)}</td
+                        : Math.round(Date.now() / 1000) -
+                              device.last_heartbeat <
+                          10
+                        ? "bg-green-500"
+                        : "bg-yellow-500"}
+                    >{formattedTime(device.last_heartbeat)}</td
                 >
                 <td>{device.last_route}</td>
                 <td
