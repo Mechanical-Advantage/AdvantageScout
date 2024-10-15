9be4212188b7177d9158eb5d66fbb28918852cdd
keith white <az2k@verizon.net>
2024-10-14 20:56:43 -0400

    Add 2018 game

---
+++ games/config/2018/GamePieceLocation.svelte
@@ -0,0 +1,20 @@
+<script>
+  import GamePiece from "./GamePiece.svelte";
+  import { gameState } from "./stores";
+  export let btnstate;
+  export let btnsize
+  export let location;
+  export let gamePieceLocation;
+
+</script>
+
+<div class="indicator absolute">
+    {#if $gameState==1}
+    <span
+    class="indicator-item indicator-middle indicator-center indicator-start badge badge-primary text-xl"
+    >{location}</span>
+    {/if}
+  <div class="absolute focus:animate-bounce">
+    <GamePiece btnstate={btnstate} btnsize={btnsize} gamePiece="Note" gamePieceLocation={location} {...location} />
+  </div>
+</div>
