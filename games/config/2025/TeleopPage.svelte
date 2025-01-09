a81c9d93d04b59c36557ac73c1e9f56e58c395b4
keith white <az2k@verizon.net>
2025-01-09 18:25:53 -0500

    lol

--- games/config/2025/TeleopPage.svelte
+++ games/config/2025/TeleopPage.svelte
@@ -6,7 +6,7 @@
   import Parked from "./Parked.svelte";
   import { teleDataLog } from "./stores";
   import CurrentTeam from "./CurrentTeam.svelte";
-  import OnStage from "./OnStage.svelte";
+  import OnStage from "./DeepClimb.svelte";
 </script>
 
 <!-- <ScoringGrid /> -->
