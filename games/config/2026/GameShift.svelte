0f9bace618e3686a5e2cdcd495ee4773e059c921
Gavin Murphy <gavintmurphy2011+frc@gmail.com>
2026-01-24 15:30:40 -0500

    Add animation for shift button

--- games/config/2026/GameShift.svelte
+++ games/config/2026/GameShift.svelte
@@ -1,8 +1,9 @@
 
 <script>
-    import { gameData, gameState, gameShift } from "./stores";
+    import { gameData, gameState, gameShift, shiftTimerStarted } from "./stores";
     let shifts = ["Transition", "Shift 1", "Shift 2", "Shift 3", "Shift 4", "Endgame"];
     let colors = ["btn-red", "btn-orange", "btn-yellow", "btn-green", "btn-blue", "btn-purple"];
+    let animateButton = false;
     function handleClick() {
 
         if ($gameShift === 5) {
@@ -12,88 +13,100 @@
         }
 
     }
+    if (!$shiftTimerStarted) {
+      setTimeout(() => {
+        animateButton = true;
+        setTimeout(() => animateButton = false, 2000);
+      }, 10000)
+      
+      const interval = setInterval(() => {
+        animateButton = true;
+        setTimeout(() => animateButton = false, 2000);
+      }, 30000)
+    }
+    $shiftTimerStarted = true;
     let buttonSize = 'btn-small';
-  </script>
-  
-  <style>
-    .btn {
-      padding: 12px 24px;
-      font-size: 24px;
-    }
-  
-    .btn-disabled {
-      opacity: 0.5;
-      cursor: not-allowed;
-    }
-    .btn-primary {
-      background-color: rgb(0, 51, 255);
-      color: white;
-    }
-    .btn-red {
-      background-color: rgb(255, 0, 0);
-      color: white;
-    }
+</script>
 
-    .btn-orange {
-      background-color: rgb(255, 150, 0);
-      color: white;
-    }
+<style>
+  .btn {
+    padding: 12px 24px;
+    font-size: 24px;
+  }
 
-    .btn-yellow {
-      background-color: rgb(255, 255, 0);
-      color: black; 
-    }
+  .btn-disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+  .btn-primary {
+    background-color: rgb(0, 51, 255);
+    color: white;
+  }
+  .btn-red {
+    background-color: rgb(255, 0, 0);
+    color: white;
+  }
 
-    .btn-green {
-      background-color: rgb(25, 200, 25);
-      color: white;
-    }
+  .btn-orange {
+    background-color: rgb(255, 150, 0);
+    color: white;
+  }
 
-    .btn-blue {
-      background-color: rgb(0, 51, 255);
-      color: white;
-    }
+  .btn-yellow {
+    background-color: rgb(255, 255, 0);
+    color: black; 
+  }
 
-    .btn-purple {
-      background-color: rgb(160, 0, 160);
-      color: white;
-    }
+  .btn-green {
+    background-color: rgb(25, 200, 25);
+    color: white;
+  }
 
-    .btn-primary {
-      background-color: rgb(0, 51, 255);
-      color: white;
-    }
+  .btn-blue {
+    background-color: rgb(0, 51, 255);
+    color: white;
+  }
 
-    .btn-red:hover {
-      background-color: rgb(200, 0, 0);
-      color: white;
-    }
+  .btn-purple {
+    background-color: rgb(160, 0, 160);
+    color: white;
+  }
 
-    .btn-orange:hover {
-      background-color: rgb(200, 117, 0);
-      color: white;
-    }
+  .btn-primary {
+    background-color: rgb(0, 51, 255);
+    color: white;
+  }
 
-    .btn-yellow:hover {
-      background-color: rgb(200, 200, 0);
-      color: black; 
-    }
+  .btn-red:hover {
+    background-color: rgb(200, 0, 0);
+    color: white;
+  }
 
-    .btn-green:hover {
-      background-color: rgb(23, 169, 23);
-      color: white;
-    }
+  .btn-orange:hover {
+    background-color: rgb(200, 117, 0);
+    color: white;
+  }
 
-    .btn-blue:hover {
-      background-color: rgb(0, 40, 200);
-      color: white;
-    }
+  .btn-yellow:hover {
+    background-color: rgb(200, 200, 0);
+    color: black; 
+  }
 
-    .btn-purple:hover {
-      background-color: rgb(105, 0, 105);
-      color: white;
-    }
-  </style>
+  .btn-green:hover {
+    background-color: rgb(23, 169, 23);
+    color: white;
+  }
 
-<button class="btn {colors[$gameShift]}"
+  .btn-blue:hover {
+    background-color: rgb(0, 40, 200);
+    color: white;
+  }
+
+  .btn-purple:hover {
+    background-color: rgb(105, 0, 105);
+    color: white;
+  }
+</style>
+
+<button class="btn {animateButton ? "animate-bounce" : ""} {colors[$gameShift]}"
      on:click={handleClick}>{shifts[$gameShift]}</button>
--- games/config/2026/stores.js
+++ games/config/2026/stores.js
@@ -3,21 +3,22 @@ import { writable } from 'svelte/store';
 export const liveGamepiece = writable("Fuel");
 export const liveLocation = writable(0);
 export const floorPickup = writable(0);
-export const gameState = writable(0)
-export const autoState = writable(0)
-export const autoDataLog = writable([])
-export const teleDataLog = writable([])
-export const reversedAlliance = writable(0)
-export const selectedCommunity = writable(0) //blue is 0, red is 1
-export const displayText = writable(["x", " "]) //idek
-export const undoDisplayCondition = writable(false)
-export const uploadState = writable(0)
-export const currentTeam = writable(0)
-export const onStageButton = writable(0)
+export const gameState = writable(0);
+export const autoState = writable(0);
+export const autoDataLog = writable([]);
+export const teleDataLog = writable([]);
+export const reversedAlliance = writable(0);
+export const selectedCommunity = writable(0); //blue is 0, red is 1
+export const displayText = writable(["x", " "]); //idek
+export const undoDisplayCondition = writable(false);
+export const uploadState = writable(0);
+export const currentTeam = writable(0);
+export const onStageButton = writable(0);
 export const fuelCycleCountSuccess = writable(0);
 export const fuelCycleCountFail = writable(0);
 export const fuelButtonSpeed = writable(200);
 export const gameShift = writable(0);
+export const shiftTimerStarted = writable(false);
 
 export const gameData = writable({
 	AllianceColor: 0,
